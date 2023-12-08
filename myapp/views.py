01dff500ff306c71f16d0f0c853245b7222a210a
inirav <niravpanchal108@gmail.com>
2023-12-08 17:14:31 +0530

    minor changes

--- myapp/views.py
+++ myapp/views.py
@@ -7,10 +7,13 @@ def predict(request):
     if request.method == 'POST':
         text = request.POST.get('mytext')
         model = torch.jit.load(settings.MODEL_PATH)
-        inputs = torch.tensor([text])
-        outputs = model(inputs)
-        prediction = outputs.argmax(dim=-1).item()
-        print(f'predictions are {prediction}')
-        return render(request, 'predictions.html', {'prediction': prediction})
+        model.eval()
+        inputs = torch.tensor([15, 24, 38 ])
+        all_encoder_layers, pooled_output = model(inputs)
+        # inputs = torch.tensor([text])
+        # outputs = model(inputs)
+        # prediction = outputs.argmax(dim=-1).item()
+        print(f'predictions are {all_encoder_layers} and {pooled_output}')
+        return render(request, 'predictions.html', {'prediction': ''})
     else:
         return render(request, 'predictions.html')
